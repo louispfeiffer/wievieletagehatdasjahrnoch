c4c266e0676b088cc5809f9c421f6fadaefdb420
louispfeiffer <pfeifferlouis2006@gmail.com>
2024-03-09 14:27:36 +0100

    diigggiii

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,21 +1,79 @@
 import * as React from "react"
 import Navbar from "./navbar"
 import Footer from "./footer"
-import DaysLeft from "./dayleft"
-
-import Img from "../images/clockonwrist.webp"
 
 const ImpressumPage = () => {
-  return (
-    <html>
-      <body>
-          <Navbar></Navbar>
+    return (
+        <html>
+            <body>
+                <Navbar></Navbar>
 
+                <div class="container mx-auto px-4 mt-8">
+                    <h1 class="text-xl font-bold text-center my-5">Impressum</h1>
+                    <div class="bg-white p-6 rounded-lg shadow-md">
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Angaben gem. § 5 TMG:</h2>
+                            <p>
+                                Louis Pfeiffer
+                                Birkbuschgarten 2
+                                12167, Berlin
+                            </p>
 
-        <Footer></Footer>
-      </body>
-    </html>
-  )
+                            <div class="mb-4">
+                                <h2 class="text-lg font-semibold">Kontaktaufnahme:</h2>
+                                <p>Telefon: 0176 72782147<br></br>E-Mail: <a href="mailto:louispfeiffer@info.com" class="text-blue-500">louispfeiffer@info.com</a></p>
+                            </div>
+                            <div class="mb-4">
+                                <h2 class="text-lg font-semibold">Umsatzsteuer-ID</h2>
+                                <p>Umsatzsteuer-Identifikationsnummer gem. § 27 a Umsatzsteuergesetz:<br></br>DE XXX XXX XXX</p>
+                            </div>
+                            <div class="mb-4">
+                                <h2 class="text-lg font-semibold">Haftungsausschluss – Disclaimer:</h2>
+                                <p>Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt.
+                                    Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
+                                    Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+                                    Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+                                    übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen,
+                                    die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
+                                    Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+                                    Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt
+                                    der Kenntniserlangung einer konkreten Rechtsverletzung möglich.
+                                    Bei Bekanntwerden von den o.g. Rechtsverletzungen werden wir diese Inhalte unverzüglich entfernen.
+                                </p>
+                            </div>
+                            <div class="mb-4">
+                                <h2 class="text-lg font-semibold">Haftungsbeschränkung für externe Links</h2>
+                                <p>Unsere Webseite enthält Links auf externe Webseiten Dritter.
+                                    Auf die Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir keinen Einfluss.
+                                    Daher können wir für die „externen Links“ auch keine Gewähr auf Richtigkeit der Inhalte übernehmen.
+                                    Für die Inhalte der externen Links sind die jeweilige Anbieter oder Betreiber (Urheber) der Seiten verantwortlich.
+                                    Die externen Links wurden zum Zeitpunkt der Linksetzung auf eventuelle Rechtsverstöße überprüft
+                                    und waren im Zeitpunkt der Linksetzung frei von rechtswidrigen Inhalten. Eine ständige inhaltliche
+                                    Überprüfung der externen Links ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht möglich.
+                                    Bei direkten oder indirekten Verlinkungen auf die Webseiten Dritter, die außerhalb unseres Verantwortungsbereichs liegen,
+                                    würde eine Haftungsverpflichtung ausschließlich in dem Fall nur bestehen, wenn wir von den Inhalten Kenntnis erlangen und
+                                    es uns technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern.
+                                    Diese Haftungsausschlusserklärung gilt auch innerhalb des eigenen Internetauftrittes „Name Ihrer Domain“
+                                    gesetzten Links und Verweise von Fragestellern, Blogeinträgern, Gästen des Diskussionsforums. Für illegale,
+                                    fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung
+                                    solcherart dargestellten Informationen entstehen, haftet allein der Diensteanbieter der Seite,
+                                    auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.
+                                    Werden uns Rechtsverletzungen bekannt, werden die externen Links durch uns unverzüglich entfernt.</p>
+                            </div>
+                            <div class="mb-4">
+                                <h2 class="text-lg font-semibold">Urheberrecht</h2>
+                                <p>Die auf unserer Webseite veröffentlichen Inhalte und Werke unterliegen dem deutschen Urheberrecht (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf) . Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung des geistigen Eigentums in ideeller und materieller Sicht des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der vorherigen schriftlichen Zustimmung des jeweiligen Urhebers i.S.d. Urhebergesetzes (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf ). Downloads und Kopien dieser Seite sind nur für den privaten und nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer Webseite nicht von uns erstellt wurden, sind die Urheberrechte Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich gemacht. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte unverzüglich entfernen.
+                                    Dieses Impressum wurde freundlicherweise von jurarat.de zur Verfügung gestellt.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <Footer></Footer>
+            </body>
+        </html>
+    )
 }
 
 export default ImpressumPage
